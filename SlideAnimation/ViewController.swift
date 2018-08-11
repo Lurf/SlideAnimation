69d2e862ef38dcc7c38e6e61e76e9be3012c091b
Lurf <lurf@icloud.com>
2018-08-11 15:03:31 +0900

    Initial Commit

---
+++ SlideAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideAnimation
+//
+//  Created by Lurf on 2018/08/11.
+//  Copyright © 2018年 ykawamur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
